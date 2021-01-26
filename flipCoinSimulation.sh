93750db1ad4fe71849aae2952a8435066b9068b3
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-01-26 22:06:31 +0530

    REFACTOR.Loop through multiple time of  flipping a coin and show number of times head and tails has won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,8 +1,10 @@
-#!bin/bash -x
-coinFlip=$((RANDOM %2))
-if (( ($coinFlip==0) )); then
-	echo "Tails"
-else
-	echo "Heads"
-fi
-
+#!/bin/bash -x
+for (( count=1 ; count<=5 ; count++ ))
+do
+        coinFlip=$((RANDOM %2))
+        if (( ($coinFlip==0) )); then
+                echo "Tails"
+        else
+                echo "Heads"
+        fi
+done
